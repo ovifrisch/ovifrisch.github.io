7675ef71206c9084e22303caeca79169e44b2a0f
Ovi Fritsch <ovidiu.fritsch@gmail.com>
2018-03-01 22:48:57 -0800

    temp

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-function sayHello(name) {
-    document.getElementById("result").innerHTML = 'Hello ' +
-    name + '!';
+function processForm() {
+    
+    var temperature = Number(document.tempForm.temp.value);
+    var tempType;
+    var result;
+
+    for (var i=0; i < document.tempForm.choice.length; i++) {
+        
+        if (document.tempForm.choice[i].checked) {
+            tempType = document.tempForm.choice[i].value;
+        }
+    }
+
+    if (tempType == 'fahrenheit') {
+        result = temperature * 9/5 + 32;
+    }
+    
+    else {
+        result = (temperature -  32)  *  5/9;
+    }
+    
+    document.tempForm.resultField.value = result;
 }
