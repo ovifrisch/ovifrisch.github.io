6e03fa0462d92ad952dbbcfe7d317abf8250909c
Ovidiu Fritsch <33567249+ovifrisch@users.noreply.github.com>
2018-03-03 17:35:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-function processForm() {
-    
-    var temperature = Number(document.tempForm.temp.value);
-    var tempType;
-    var result;
+var grid_set = 0; //gets enabled when the user clicks set
+var grid;
 
-    for (var i=0; i < document.tempForm.choice.length; i++) {
-        
-        if (document.tempForm.choice[i].checked) {
-            tempType = document.tempForm.choice[i].value;
+function solve() {
+    if (!grid_set)
+        return;
+    var squares = document.getElementsByClassName("num_in");
+    
+
+}
+
+function set() {
+    grid_set = 1;
+    var squares = document.getElementsByClassName("num_in");
+
+    grid = new Grid(3);
+    
+    var att;
+    for (i = 0; i < squares.length; i++) {
+        /* Disable text entry */
+        att = document.createAttribute("disabled");
+        squares[i].setAttributeNode(att);
+
+
+    }
+}
+
+function reset() {
+    grid_set = 0;
+    var squares = document.getElementsByClassName("num_in");
+
+    for (i = 0; i < squares.length; i++) {
+        squares[i].removeAttribute("disabled");
+        squares[i].value = "";
+        squares[i].parentElement.style.border = "1px solid black";
+    }
+}
+
+function square_click(row, col) {
+    if (!grid_set)
+        return;
+
+    var squares = document.getElementsByClassName("num_in");
+    var row_class = "row" + row;
+    var col_class = "col" + col;
+    console.log(row_class);
+    for (i = 0; i < squares.length; i++) {
+        if (squares[i].classList.contains(row_class) && squares[i].classList.contains(col_class)) {
+            squares[i].parentElement.style.border = "2px solid blue";
         }
-    }
 
-    if (tempType == 'fahrenheit') {
-        result = temperature * 9/5 + 32;
     }
-    
-    else {
-        result = (temperature -  32)  *  5/9;
-    }
-    
-    document.tempForm.resultField.value = result;
 }
